38542adb362ac6072a7ab49c3ecf756d04efaf01
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-08-07 10:57:12 +0900

    Define associations between models and export

---
+++ lib/models/index.ts
@@ -0,0 +1,13 @@
+import User from './User'
+import GithubUserProfile from './GithubUserProfile'
+import Post from './Post'
+
+User.hasOne(GithubUserProfile)
+GithubUserProfile.belongsTo(User, {
+  foreignKey: {
+    name: 'userId',
+    allowNull: false,
+  },
+})
+
+export { User, GithubUserProfile, Post }
