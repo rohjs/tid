96df8e425c8e01c5dded5ec02d815df811716283
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-08-07 10:56:34 +0900

    Create a User model

---
+++ lib/models/User.ts
@@ -0,0 +1,46 @@
+import { DataTypes, Model } from 'sequelize'
+import sequelize from '../sequelize'
+import { fixSequelizeModel } from '../db'
+
+interface UserAttributes {
+  id: number
+  name: string
+}
+
+interface UserCreationAttributes extends Omit<UserAttributes, 'id'> {}
+
+class User extends Model<UserAttributes, UserCreationAttributes>
+  implements UserAttributes {
+  constructor(...args: any[]) {
+    super(...args)
+    fixSequelizeModel(new.target, this)
+  }
+
+  public readonly id!: number
+  public name!: string
+  public githubToken!: string
+  public githubId!: string
+  public createdAt!: Date
+  public updatedAt!: Date
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    sequelize,
+    modelName: 'User',
+  }
+)
+
+export default User
