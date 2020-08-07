683519bfa6919c0ce7502ea5f4e7b58dbc73720d
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-08-07 17:58:42 +0900

    Add logging option to be false

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, '', {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize('tid', 'root', '', {
+  host: 'localhost',
   dialect: 'postgres',
+  logging: false,
 })
 
 export default sequelize
